e1e7e48b57632e48b2f460a29fe6a251dbc4493c
PinRay22 <102412799+PinRay22@users.noreply.github.com>
2023-01-05 21:11:27 +0800

    Add files via upload

---
+++ systemanalyst/urls.py
@@ -0,0 +1,34 @@
+"""systemanalyst URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from myapp import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.main),
+    path('index/', views.index),
+    path('alt_view/', views.alt_view),
+    path('alt_member/', views.alt_member),
+    path('vmember/', views.member_view ),
+    path('signup_view/', views.signup_view ),
+    path('signup/', views.signup ),
+    path('logout/', views.logout ),
+    path('login_p/', views.login_p ),
+    path('login/', views.login_view),
+    path("news/", include("news.urls")),
+    
+]
